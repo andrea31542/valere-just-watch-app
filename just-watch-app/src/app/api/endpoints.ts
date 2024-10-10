225eaea9c07d8aaf5c276c72278a5752a001cc55
Andrea Vrljić <andrea.vrljic@deegloo.com>
2024-10-10 16:40:35 +0200

    Fix scroll horizontal

--- just-watch-app/src/app/api/endpoints.ts
+++ just-watch-app/src/app/api/endpoints.ts
@@ -1,7 +1,7 @@
 export const endpoints = {
   nowPlaying: `movie/now_playing`,
-  topRated: `movie/top_rated?language=en-US&page=1`,
-  genres: 'genre/movie/list?language=en',
-  movieByGenre: '/discover/movie?language=en-US&page=1&sort_by=popularity.desc',
+  topRated: `movie/top_rated?page=1`,
+  genres: 'genre/movie/list?',
+  movieByGenre: '/discover/movie?page=1&sort_by=popularity.desc',
   movieDetails: '/movie/',
 };
--- just-watch-app/src/app/components/MovieList.tsx/HorizontalScrollContainer.tsx
+++ just-watch-app/src/app/components/MovieList.tsx/HorizontalScrollContainer.tsx
@@ -1,10 +1,28 @@
 'use client';
-import { ReactNode, useRef } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import IconButton from '../IconButton';
 import icons from '@/app/icons';
 
 const HorizontalContainer = ({ children }: { children: ReactNode }) => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const [showScrollButtons, setShowScrollButtons] = useState({
+    left: false,
+    right: false,
+  });
+
+  const checkScrollButtonsVisibility = () => {
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef.current;
+      const canScrollLeft = container.scrollLeft > 0;
+      const canScrollRight =
+        container.scrollWidth > container.scrollLeft + container.clientWidth;
+
+      setShowScrollButtons({
+        left: canScrollLeft,
+        right: canScrollRight,
+      });
+    }
+  };
 
   const scroll = (direction: 'left' | 'right') => {
     if (scrollContainerRef.current) {
@@ -16,25 +34,43 @@ const HorizontalContainer = ({ children }: { children: ReactNode }) => {
     }
   };
 
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      checkScrollButtonsVisibility();
+      scrollContainer.addEventListener('scroll', checkScrollButtonsVisibility);
+      return () => {
+        scrollContainer.removeEventListener(
+          'scroll',
+          checkScrollButtonsVisibility
+        );
+      };
+    }
+  }, [children]);
+
   return (
     <div className='flex flex-col gap-4 relative'>
       <div className='relative flex items-center'>
-        <IconButton
-          onClick={() => scroll('left')}
-          className='h-full absolute left-0 z-10 bg-[#060d17] bg-opacity-60  p-2'
-          icon={icons.chevronLeft}
-        />
+        {showScrollButtons.left && (
+          <IconButton
+            onClick={() => scroll('left')}
+            className='h-full absolute left-0 z-10 bg-[#060d17] bg-opacity-60 p-2 text-[var(--color-light-tint)]'
+            icon={icons.chevronLeft}
+          />
+        )}
         <div
           ref={scrollContainerRef}
           className='flex gap-4 overflow-x-auto scrollbar-hide scroll-smooth'
         >
           {children}
         </div>
-        <IconButton
-          onClick={() => scroll('right')}
-          className='absolute right-0 z-10  bg-[#060d17] bg-opacity-60 p-2 h-full flex items-center justify-center'
-          icon={icons.chevronRight}
-        />
+        {showScrollButtons.right && (
+          <IconButton
+            onClick={() => scroll('right')}
+            className='absolute right-0 z-10 bg-[#060d17] bg-opacity-60 p-2 h-full flex items-center justify-center text-[var(--color-light-tint)]'
+            icon={icons.chevronRight}
+          />
+        )}
       </div>
     </div>
   );
